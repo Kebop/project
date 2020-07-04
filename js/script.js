5d3eefcb5cdfd648ccf782fd7a52b5dcc5d2bda2
alex <example@gmail.com>
2020-07-04 14:51:31 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const answers = [];
+
+answers [0] = prompt('Ваше имя?', '');
+answers [1] = prompt('Ваша фамилия?', '');
+answers [2] = prompt('Сколько вам лет?', '');
+
+document.write(answers);
+//const answer = prompt("Вам есть 18?", "18");
+//console.timeLog(answer);
+//const result = confirm("Are you here?");
+//console.log(result);
+//alert ('Hello');
+/*const obj = {
+    a: 50
+};
+
+obj.a = 10;
+
+console.log(obj);
+*/
+
+/*const obj = {
+    name: 'jon',
+    age: 25,
+    isMarried: false
+};
+console.log(obj.name);*/
